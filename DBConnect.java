5cf7c4a79bdd9a2749f8291910be6b56514ce09c
rahul-naik92 <teamrahul.naik@gmail.com>
2023-04-07 11:01:25 +0530

    resolved the conflict-2

--- DBConnect.java
+++ DBConnect.java
@@ -1,6 +1,12 @@
+A
+B
 
+<<<<<<< HEAD
 mmmmmmm
 
+=======
+AIM in the Dev branch
+>>>>>>> 95799ed... updated the DBConnect file with password
 mmmmmmmmmmmmmmmmmmmmmmmmmmm dev branc
 
 Hi, im updating the codeHey there!! Its DBCOnnect File.
